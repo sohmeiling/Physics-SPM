8cc78b6e057fe1c6fa8f933ea964724612517a20
Soh Mei Ling <meigemini.91@gmail.com>
2021-09-04 23:37:13 +0800

    upload main file

--- include/script.js
+++ include/script.js
@@ -1,7 +1,7 @@
 <script>
 $( document ).ready(function() {
   var cite = ' ';
-  var psyteachr = ' <a href="https://psyteachr.github.io/"><img src="images/logos/psyteachr_logo.png" style="height: 31px; color: white;" alt="psyTeachR: Reproducible Research" /></a>';
+  var psyteachr = ' <a href="https://sohmeiling.github.io/"><img src="images/atom-icon.png" style="height: 31px; color: white;" alt="Open Science: Physics Textbook" /></a>';
   var license = ' <a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/" target="blank"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a>';
 
   $("footer div.row div:eq(1) p").html(
@@ -9,4 +9,3 @@ $( document ).ready(function() {
   );
 });
 </script>
-
